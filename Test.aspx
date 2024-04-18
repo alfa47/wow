fefefedbe60d33f53eeb87b58e2dae987444860a
alfa47 <atlas1926@gmail.com>
2024-04-18 20:34:06 +0100

    Add files via upload

--- Test.aspx
+++ Test.aspx
@@ -1,25 +1,44 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="RZASolution.Test" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="test123.Test" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
 </head>
-<body style="height: 265px">
+<body>
     <form id="form1" runat="server">
-    <div>
-        <asp:TextBox ID="txtUsername" runat="server" />
-        <asp:Button ID="DeleteUserbtn" runat="server" Text="Using Username to delete" onclick="UsernameDelete"/>
-        <asp:Button ID="DeleteIDbtn" runat="server" Text="Using ID to delete" onclick="DeleteIDbtn_Click"/>
-        <asp:Button ID="Updatebtn" runat="server" Text="Update database" onclick="Updatebtn_Click"/>
-        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true" />
+        <div>
+            <asp:TextBox ID="txtUsername" runat="server" />
+            <asp:TextBox ID="txtPassword" runat="server" />
+            <asp:Button ID="Addrowbtn" runat="server" text="add" OnClick="Addrowbtn_Click" />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+                DataKeyNames="ID" ShowFooter="true"  OnRowEditing="GridView1_RowEditing" 
+                OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+                OnRowUpdating="GridView1_RowUpdating" OnRowCommand="GridView1_RowCommand" OnRowDeleting="GridView1_RowDeleting"  >
+                <Columns>
+                    <asp:BoundField DataField="ID" HeaderText="ID" ReadOnly="true" />
+                    <asp:BoundField DataField="Username" HeaderText="Name" />
+                    <asp:BoundField DataField="Password" HeaderText="Password" />
+                    <asp:TemplateField HeaderText="Edit">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkEdit" runat="server" CausesValidation="False" CommandName="Edit" Text="Edit"></asp:LinkButton>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:LinkButton ID="lnkUpdate" runat="server" CausesValidation="True" CommandName="Update" Text="Update"></asp:LinkButton>
+                            <asp:LinkButton ID="lnkCancel" runat="server" CausesValidation="False" CommandName="Cancel" Text="Cancel"></asp:LinkButton>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Delete">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkDelete" runat="server" CausesValidation="False" CommandName="Delete" Text="Delete" OnClientClick="return confirm('Are you sure you want to delete this record?');"></asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
 
-        <br />
-        <asp:Label ID="Successlbl" runat="server" ForeColor="Green"/>
-        <br />
-        <asp:Label ID="Errorlbl" runat="server" ForeColor="Red"/>
-    </div>
+        </div>
     </form>
 </body>
 </html>
-    
--- Test.aspx.cs
+++ Test.aspx.cs
@@ -1,112 +1,170 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.OleDb;
-using System.Configuration;
-using System.Data;
-using System.Web.DynamicData;
-using System.ComponentModel;
+using System.Web.Services.Description;
 
-namespace RZASolution
+namespace test123
 {
     public partial class Test : System.Web.UI.Page
     {
         string connectionString = ConfigurationManager.ConnectionStrings["RZAData"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["LoggedIn"] == null)
-            {
-                Response.Redirect("Login.aspx");
-            }
             if (!IsPostBack)
             {
-                GridGeneration();
+                BindGridView();
             }
 
         }
-        protected void UsernameDelete(object sender, EventArgs e)
+        protected void BindGridView()
         {
-            DeleteUsingUsername();
-        }
-        protected void DeleteIDbtn_Click(object sender, EventArgs e)
-        {
-            DeleteUsingID();
-        }
-        protected void Updatebtn_Click(object sender, EventArgs e)
-        {
-            GridGeneration();
-
-
-        }
-
-        private void DeleteUsingUsername()
-        {
-            string username = txtUsername.Text;
-            using(OleDbConnection conn = new OleDbConnection(connectionString))
+            using (OleDbConnection con = new OleDbConnection(connectionString))
             {
-                using (OleDbCommand command = new OleDbCommand("DELETE FROM Users WHERE Username = @Username", conn))
-                {
-                    command.Parameters.AddWithValue("@Username", username);
-                    conn.Open();
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                    {
-                        Successlbl.Text = "Deleted";
-                    }
-                    else
-                    {
-                        Errorlbl.Text = "Failed";
-                    }
-                }
+                OleDbCommand cmd = new OleDbCommand("SELECT ID, Username, Password FROM Users", con);
+                con.Open();
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
             }
-            GridGeneration();
         }
-        private void DeleteUsingID()
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
         {
-            string ID = txtUsername.Text;
-            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            // Set GridView edit index
+            GridView1.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            // Cancel editing
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            // Update row
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int ID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string name = ((TextBox)row.Cells[1].Controls[0]).Text;
+            string password = ((TextBox)row.Cells[2].Controls[0]).Text;
+           
+
+            using (OleDbConnection con = new OleDbConnection(connectionString))
             {
-                using (OleDbCommand command = new OleDbCommand("DELETE FROM Users WHERE ID = @ID", conn))
-                {
-                    command.Parameters.AddWithValue("@ID", ID);
-                    conn.Open();
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                    {
-                        Successlbl.Text = "Deleted";
-                        Errorlbl.Visible = false;
-                
-                    }
-                    else
-                    {
-                        Errorlbl.Text = "Failed";
-                        Successlbl.Visible = false;
-                    }
-                }
+                OleDbCommand cmd = new OleDbCommand("UPDATE Users SET Username=@Name, [Password]=@Password WHERE ID=@ID", con);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Password", password);
+                cmd.Parameters.AddWithValue("@ID", ID);
+                ScriptManager.RegisterStartupScript(this, GetType(), "alert", $"alert('{cmd.CommandText}');", true);
+                con.Open();
+                cmd.ExecuteNonQuery();
             }
-            GridGeneration();
+
+            // Exit edit mode
+            GridView1.EditIndex = -1;
+            BindGridView();
+
         }
-        private void GridGeneration()
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
         {
-            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            if (e.CommandName == "Insert")
             {
-                using (OleDbCommand command = new OleDbCommand("SELECT * FROM Users", conn)) 
+                TextBox txtNewName = GridView1.FooterRow.FindControl("txtNewName") as TextBox;
+                TextBox txtNewPassword = GridView1.FooterRow.FindControl("txtNewPassword") as TextBox;
+
+                if (txtNewName != null && txtNewPassword != null)
                 {
-                    conn.Open();
-                    OleDbDataAdapter adapter = new OleDbDataAdapter(); 
-                    DataTable dataTable = new DataTable();
-                    adapter.SelectCommand = command;
-                    adapter.Fill(dataTable);
-                    GridView1.DataSource = dataTable;
-                    GridView1.DataBind();
-                    Successlbl.Text = "Updated!";
+                    string name = txtNewName.Text;
+                    string password = txtNewPassword.Text;
+
+                    InsertData(name, password);
+
+                    // Rebind the GridView to show the newly inserted data
+                    BindGridView();
                 }
             }
         }
+        protected void InsertData(string name, string password)
+        {
+            using (OleDbConnection con = new OleDbConnection(connectionString))
+            {
+                OleDbCommand cmd = new OleDbCommand("INSERT INTO Users (Username, [Password]) VALUES (@Name, @Password)", con);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Password", password);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
 
 
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int ID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+            using (OleDbConnection con = new OleDbConnection(connectionString))
+            {
+                OleDbCommand cmd = new OleDbCommand("DELETE FROM Users WHERE ID=@ID", con);
+                cmd.Parameters.AddWithValue("@ID", ID);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            BindGridView();
+        }
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.Footer)
+            {
+                TableCell cell = new TableCell();
+                cell.ColumnSpan = GridView1.Columns.Count;
+
+                TextBox txtNewUsername = new TextBox();
+                txtNewUsername.ID = "txtNewUsername";
+                txtNewUsername.CssClass = "form-control";
+                txtNewUsername.Attributes["placeholder"] = "New Username";
+                cell.Controls.Add(txtNewUsername);
+
+                TextBox txtNewPassword = new TextBox();
+                txtNewPassword.ID = "txtNewPassword";
+                txtNewPassword.CssClass = "form-control";
+                txtNewPassword.Attributes["placeholder"] = "New Password";
+                cell.Controls.Add(txtNewPassword);
+
+                LinkButton lnkInsert = new LinkButton();
+                lnkInsert.ID = "lnkInsert";
+                lnkInsert.CausesValidation = true;
+                lnkInsert.CommandName = "Insert";
+                lnkInsert.Text = "Add";
+                lnkInsert.CssClass = "btn btn-primary";
+                cell.Controls.Add(lnkInsert);
+
+                e.Row.Cells.Add(cell);
+            }
+        }
+
+        protected void Addrowbtn_Click(object sender, EventArgs e)
+        {
+            string txtNewName = txtUsername.Text;
+            string txtNewPassword = txtPassword.Text;
+
+            if (txtNewName != null && txtNewPassword != null)
+            {
+                string name = txtNewName.ToString();
+                string password = txtNewPassword.ToString();
+
+                InsertData(name, password);
+
+                // Rebind the GridView to show the newly inserted data
+                BindGridView();
+            }
+        }
     }
 }
