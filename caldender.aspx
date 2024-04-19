7113017b79b30e8e06b70213e875466fa5313b7a
alfa47 <atlas1926@gmail.com>
2024-04-19 08:21:53 +0100

    Add files via upload
    added calender aspx database added bookedates

---
+++ caldender.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="caldender.aspx.cs" Inherits="RZATest.caldender" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Book a Date</h2>
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label><br />
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+            <asp:Button ID="btnBook" runat="server" Text="Book" OnClick="btnBook_Click" />
+        </div>
+                <div>
+            <h2>Login</h2>
+            <asp:Label ID="Label1" runat="server" ForeColor="Red"></asp:Label><br />
+            <div>
+                <label for="txtUsername">Username:</label>
+                <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <label for="txtPassword">Password:</label>
+                <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+            </div>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ caldender.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RZATest
+{
+    public partial class caldender : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["RZAData"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Initialize the calendar to show current date
+                Calendar1.SelectedDate = DateTime.Today;
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            // Update the selected date label if needed
+        }
+
+        protected void btnBook_Click(object sender, EventArgs e)
+        {
+            // Get the selected date
+            DateTime selectedDate = Calendar1.SelectedDate;
+
+            // Insert the date into the database
+            string insertQuery = "INSERT INTO BookedDates ([Date], [UserID]) VALUES (?, ?)";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Date", selectedDate);
+                    command.Parameters.AddWithValue("@UserID", Session["UserID"]);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            lblMessage.Text = "Date booked successfully!";
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+
+            // Authenticate the user
+            int userId = AuthenticateUser(username, password);
+
+            if (userId != -1)
+            {
+                // Set the user ID in Session
+                Session["UserID"] = userId;
+                lblMessage.Text = "Logged in";
+                // Redirect to the homepage or any other page
+            }
+            else
+            {
+                // Authentication failed, show error message
+                lblMessage.Text = "Invalid username or password.";
+            }
+        }
+
+        private int AuthenticateUser(string username, string password)
+        {
+            // Your connection string to the database
+            // SQL query to retrieve user ID based on username and password
+            string query = "SELECT UserID FROM Users WHERE [Username] = ? AND [Password] = ?";
+
+            int userId = -1;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    connection.Open();
+
+                    object result = command.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        userId = Convert.ToInt32(result);
+                    }
+                }
+            }
+
+            return userId;
+        }
+    }
+}
