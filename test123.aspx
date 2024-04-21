c5d7b4810354705b204a56fa012f70fb3e8d533b
alfa47 <atlas1926@gmail.com>
2024-04-21 23:34:09 +0100

    Add files via upload

---
+++ test123.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test123.aspx.cs" Inherits="RZATest.test123" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>CRUD Operations</h1>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>User ID</th>
+                        <th>Booking</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <asp:Literal ID="litDataRows" runat="server"></asp:Literal>
+                </tbody>
+            </table>
+            <div>
+                <asp:Label ID="lblErrorMessage" runat="server" ForeColor="Red"></asp:Label>
+            </div>
+            <div>
+                <label>Date:</label>
+                <asp:TextBox ID="txtDate" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <label>User ID:</label>
+                <asp:TextBox ID="txtUserID" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <label>Booking:</label>
+                <asp:TextBox ID="txtBooking" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ test123.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RZATest
+{
+    public partial class test123 : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["RZAData"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Load data on initial page load
+                LoadData();
+            }
+        }
+
+        protected void LoadData()
+        {
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                string query = "SELECT * FROM BookedDates";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+
+                conn.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+
+                // Build the HTML for data rows
+                string dataRowsHtml = "";
+                while (reader.Read())
+                {
+                    DateTime date = (DateTime)reader["Date"];
+                    string formattedDate = date.ToString("yyyy-MM-dd"); // Adjust the format as needed
+
+                    string dataRow = $"<tr><td>{formattedDate}</td><td>{reader["UserID"]}</td><td>{reader["BookingID"]}</td></tr>";
+                    dataRowsHtml += dataRow;
+                }
+
+                reader.Close();
+
+                // Assign the HTML to the Literal control
+                litDataRows.Text = dataRowsHtml;
+            }
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            // Retrieve values from TextBoxes
+            DateTime date;
+            if (!DateTime.TryParseExact(txtDate.Text, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+            { 
+                // Display error message or handle invalid date input
+                lblErrorMessage.Text = "Invalid date format. Please enter date in the format YYYY-MM-DD.";
+                return;
+            }
+
+            string userID = txtUserID.Text.Trim(); // Trim to remove leading and trailing spaces
+            if (string.IsNullOrEmpty(userID))
+            {
+                // Display error message or handle empty user ID
+                lblErrorMessage.Text = "User ID cannot be empty.";
+                return;
+            }
+
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                string query = "INSERT INTO BookedDates ([Date], [UserID]) VALUES (@Date, @UserID)";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Date", date);
+                cmd.Parameters.AddWithValue("@UserID", userID);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            lblErrorMessage.Text = "";
+
+            // Refresh data after insert
+            LoadData();
+        }
+    }
+}
