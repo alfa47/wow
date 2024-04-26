fffd3e6ee8e94c8aba6f99bb6613d29c1ced6314
alfa47 <atlas1926@gmail.com>
2024-04-26 08:22:02 +0100

    Add files via upload

--- GuestDashBoard.aspx.cs
+++ GuestDashBoard.aspx.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace RZASolution
+namespace RZATest
 {
     public partial class GuestDashBoard : System.Web.UI.Page
     {
@@ -15,6 +17,8 @@ namespace RZASolution
             {
                 Response.Redirect("Login.aspx");
             }
+            int userId = Convert.ToInt32(Session["UserID"] ?? Request.QueryString["UserID"]);
+            DisplayPoints(userId);
         }
 
         protected void LogoutBtn_Click(object sender, EventArgs e)
@@ -26,6 +30,33 @@ namespace RZASolution
         protected void BookingsBtn_Click(object sender, EventArgs e)
         {
             Response.Redirect("Bookings.aspx");
+        }
+        private void DisplayPoints(int userId)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["RZAData"].ConnectionString;
+            string query = "SELECT Points FROM Users WHERE UserID = @UserId";
+
+            using (OleDbConnection conn = new OleDbConnection(connString))
+            {
+                conn.Open();
+                using (OleDbCommand cmd = new OleDbCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+                    using (OleDbDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+             
+                            lblPoints.Text = "Your Points: " + reader["Points"].ToString();
+                        }
+                        else
+                        {
+                            lblPoints.Text = "User not found or no points available.";
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
